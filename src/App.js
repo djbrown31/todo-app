cd4da3ec155abd66b27083c949347284c1c435d0
Dan Brown <brown.danny.j@gmail.com>
2020-09-23 13:51:44 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addATodo = () => {
+    setTodos([...todos, inputText]);
+    setInputText("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>ToDo App</h1>
+
+      <form>
+        <input value={inputText} onChange={event => setInputText(event.target.value)} type="text" />
+        <button type="submit" onClick={addATodo}>Add A ToDo</button>
+      </form>
+
+      {todos.map((todo) => (
+        <h2>{todo}</h2>
+      ))}
     </div>
   );
 }
