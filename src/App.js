738327f5412558087d12e839ff661237fe3cbb4b
Dan Brown <brown.danny.j@gmail.com>
2020-09-23 14:46:39 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
 
-  const addATodo = () => {
-    setTodos([...todos, inputText]);
+  const addATodo = (event) => {
+    event.preventDefault();
+    setTodos([inputText, ...todos]);
     setInputText("");
   }
 
@@ -16,11 +18,11 @@ function App() {
 
       <form>
         <input value={inputText} onChange={event => setInputText(event.target.value)} type="text" />
-        <button type="submit" onClick={addATodo}>Add A ToDo</button>
+        <button disabled={!inputText} type="submit" onClick={addATodo}>Add A ToDo</button>
       </form>
 
       {todos.map((todo) => (
-        <h2>{todo}</h2>
+        <Todo text={todo} />
       ))}
     </div>
   );
---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Todo.css";
+
+function Todo(props) {
+    return (
+        <div className="todo">
+            <h2>{props.text}</h2>
+        </div>
+    )
+}
+
+export default Todo;
+
